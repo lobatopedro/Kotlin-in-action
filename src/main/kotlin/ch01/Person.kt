3731f2863326da631eba04044f11283d2670dc89
lobatopedro <pedrolobato1305@gmail.com>
2022-04-15 18:49:00 -0300

    first commit

---
+++ src/main/kotlin/ch01/Person.kt
@@ -0,0 +1,12 @@
+package ch01
+
+data class Person(val name: String,
+                  val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("Alice"),
+                                     Person("Bob", age = 29))
+
+    val oldest = persons.maxByOrNull { it.age ?: 0 }
+    println("The oldest is: $oldest")
+}
